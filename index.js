6d238fe874b0550024c46135772400d4f1de195e
testsnake <testsnake12@gmail.com>
2023-04-30 13:25:25 -0700

    Added settings page + controller button prompt files

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const {app, BrowserWindow, ipcMain, shell} = require('electron');
 // External modules
 const Store = require('electron-store'); // Storing user data
 const i18n = require('./src/i18n'); // i18n - Internationalization
+const winston = require('winston'); // Logging
 
 const store = new Store({name: 'TMM-User-Data'});
 
@@ -17,6 +18,23 @@ const store = new Store({name: 'TMM-User-Data'});
 // Global variables
 let win;
 let devMode = true;
+
+const logger = winston.createLogger({
+    level: 'debug',
+    format: winston.format.json(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        new winston.transports.File({ filename: 'error.log', level: 'error' }),
+        new winston.transports.File({ filename: 'combined.log' })
+    ]
+});
+
+if (process.env.NODE_ENV !== 'production') {
+    logger.add(new winston.transports.Console({
+        format: winston.format.simple()
+    }));
+}
+
 
 
 app.on('window-all-closed', function () {
@@ -50,7 +68,7 @@ const createWindow = () => {
     }
     win.loadFile('src/index.html')
         .then(() => {
-            console.log('Loaded index.html');
+            logger.debug('Loaded index.html');
             win.webContents.send('i18n-ready');  // Send custom event
         });
     win.on('closed', () => {
@@ -79,12 +97,32 @@ const setupWindow = () => {
         },
     });
     if (devMode) {
+        logger.verbose('Opening dev tools');
         sWin.webContents.openDevTools();
     }
-    sWin.loadFile('src/setup.html').then(r => console.log('Loaded settings.html'));
+    sWin.loadFile('src/setup.html').then(r => {
+        logger.debug('Loaded setup.html');
+    });
 }
 
 async function startupProcess() {
+
+}
+
+async function getGamePath() {
+    const appID = 1761390;
+
+    // TODO - check current install path
+
+    const isValid = await fs.promises.access(oldPathGame)
+        .then(() => true)
+        .catch(() => false);
+
+    if (isValid) {
+        return oldPathGame;
+    } else {
+        logger.debug(`Game path ${oldPathGame} is not valid`);
+    }
 
 }
 
@@ -98,6 +136,5 @@ app.whenReady().then(() => {
 });
 
 ipcMain.handle('translate', async (event, arg) => {
-    const result = i18n.t(arg);
-    return result;
+    return i18n.t(arg);
 });
